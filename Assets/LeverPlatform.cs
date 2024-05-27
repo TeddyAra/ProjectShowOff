cf25bdcc22fd13df0edc296e2c2bef97631568fb
alexdorne <alexandre.dorne16@gmail.com>
2024-05-27 15:30:22 +0200

    Lever and rotating platform added (prefabs)

---
+++ Assets/LeverPlatform.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverPlatform : MonoBehaviour
+{
+
+    [SerializeField] private GameObject door;
+    [SerializeField] private float rotateSpeed;
+    [SerializeField] private float maxRotation; 
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+             StartCoroutine(DoorRotation()); 
+        }
+    }
+
+    IEnumerator DoorRotation()
+    {
+        while (Mathf.Rad2Deg * door.transform.rotation.z > maxRotation)
+        {
+            Debug.Log(Mathf.Rad2Deg * door.transform.localRotation.z); 
+            door.transform.Rotate(Vector3.forward, Time.deltaTime * -rotateSpeed); 
+            yield return null; 
+        }
+    }
+
+
+}
